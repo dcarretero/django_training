b058ada919de9645ccb5182ea87523fb11594583
Daniel <daniel.carretero@gmail.com>
2020-10-17 19:02:35 +0200

    Making sense of directories structure

--- Example Basic Web App deployed in Heroku/pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import request
-from django.shortcuts import render
-
-def home(request):
-    return render(request,'home.html',{})
-
-def pages(request):
-    return render(request,'pages.html',{})
